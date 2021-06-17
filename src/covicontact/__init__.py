bdbdd0366c93bbc8b3c78499c93ae978b095efc8
Alison Paredes <20asp3@queensu.ca>
2021-06-17 09:07:03 -0400

    Returns new matrices

--- src/covicontact/__init__.py
+++ src/covicontact/__init__.py
@@ -4,7 +4,7 @@ from . external import contact_duration_matrices
 __all__ = ['mixing', 'duration']
 
 def mixing(country=None, region=None):
-    contact_matrices.main(country, region)
+    return contact_matrices.main(country, region)
 
 def duration(country=None, seconds_per_minute=None):
-    contact_duration_matrices.main(country, seconds_per_minute)
+    return contact_duration_matrices.main(country, seconds_per_minute)
